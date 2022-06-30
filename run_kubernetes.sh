5e7bb38b396c88549f116f932cd16e68ffb1cec8
Damola12345 <ajiboladamola789@gmail.com>
2022-06-30 08:58:37 +0100

    Deploy with kubernetes and save output

---
+++ run_kubernetes.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+
+# This is your Docker ID/path
+dockerpath='damola12345/ml-microservices:latest'
+
+
+# Run the Docker Hub container with kubernetes
+docker login
+kubectl run ml-microservices --image=$dockerpath --port=80
+
+
+# List kubernetes pods
+kubectl get pods
+
+# Forward the container port to a host
+kubectl port-forward ml-microservices 8000:80
+
+
+# Get logs
+kubectl logs `kubectl get pods -o=name`
